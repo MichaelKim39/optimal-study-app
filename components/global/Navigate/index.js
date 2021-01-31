73dbee5a3e30cbab194093cb022a642dde9ecf1e
Michael Kim <michaelkim39@gmail.com>
2021-01-31 12:14:58 +0000

    First commit

--- components/global/Navigate/index.js
+++ components/global/Navigate/index.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-const Navigate = ({ condition = true, route, children, serverRender = false }) => {
-    const router = useRouter()
+const Navigate = ({
+    condition = true,
+    route,
+    children,
+    serverRender = false,
+}) => {
+    const router = useRouter();
 
     useEffect(() => {
-        condition && serverRender ? window.location.pathname = route : router.push(route)
-    }, [])
+        condition && serverRender
+            ? (window.location.pathname = route)
+            : router.push(route);
+    }, []);
 
-    return (        <>
-{!condition && children}</>
-    )
-}
+    return <>{!condition && children}</>;
+};
 
-export default Navigate
+export default Navigate;
