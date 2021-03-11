58ebb2fc465784e97d73648386fc4aac7aa0a9c9
Michael Kim <michaelkim39@gmail.com>
2021-03-11 00:26:36 +0000

    Fixed up the home and about screens

--- pages/about/index.js
+++ pages/about/index.js
@@ -4,6 +4,8 @@ import { log } from '@/utils/logger';
 import { resolveAuth } from '@/actions/user';
 import withAuthSSR from '@/hoc/withAuthSSR';
 
+import styles from './About.module.scss';
+
 import DefaultLayout from '@/components/layouts/DefaultLayout';
 import PageLayout from '@/components/layouts/PageLayout';
 
@@ -11,8 +13,31 @@ const About = ({ userInfo, text, error }) => {
     return (
         <DefaultLayout userInfo={userInfo} userLoading={false}>
             <PageLayout pageTitle='About Us'>
-                <h3>Hello {userInfo.nickname}! Here's something about us.</h3>
-                <p>{error ? error : text}</p>
+                <h3 className={styles.header}>
+                    Hello {userInfo.nickname}! Here's something about us.
+                </h3>
+                <br />
+
+                <p className={styles.text}>
+                    It is often assumed that popular techniques to learn and
+                    revise information such as re-reading and note taking are
+                    the most efficient ways to learn. With the seeming increase
+                    of competition in the work and educational culture in the UK
+                    it seems struggling to revise and perform well in exams and
+                    assessments has become a commonplace problem. Yet
+                    shockingly, recent discoveries and experimental studies in
+                    the optimal learning strategies have proven there is much
+                    more depth to the “best way to learn” than meets the eye.
+                    <br />
+                    <br />
+                    Karpicke and Blunt (2011) is just one of the many pieces of
+                    evidence that suggest alternative methods such as active
+                    recall and spaced repetition are far more effective in
+                    reality, boosting exam scores by a maximum of 40% in some
+                    cases. This suggests that there is a concerning lack of
+                    awareness concerning “how to study”, a possible cause of the
+                    rapidly increasing levels of student anxiety and stress
+                </p>
             </PageLayout>
         </DefaultLayout>
     );
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 
+import styles from './Home.module.scss';
+
 import DefaultLayout from '@/components/layouts/DefaultLayout';
 import PageLayout from '@/components/layouts/PageLayout';
 
@@ -8,52 +10,30 @@ const Home = ({ userInfo, userLoading }) => {
     return (
         <DefaultLayout userInfo={userInfo} userLoading={userLoading}>
             <PageLayout pageTitle='Home Page'>
-                <h3>Mission Statement</h3>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing software
-                    like Aldus PageMaker including versions of Lorem Ipsum. Why
-                    do we use it? It is a long established fact that a reader
-                    will be distracted by the readable content of a page when
-                    looking at its layout. The point of using Lorem Ipsum is
-                    that it has a more-or-less normal distribution of letters,
-                    as opposed to using 'Content here, content here', making it
-                    look like readable English. Many desktop publishing packages
-                    and web page editors now use Lorem Ipsum as their default
-                    model text, and a search for 'lorem ipsum' will uncover many
-                    web sites still in their infancy. Various versions have
-                    evolved over the years, sometimes by accident, sometimes on
-                    purpose (injected humour and the like).
+                <h3 className={styles.header}>Mission Statement</h3>
+                <p className={styles.text}>
+                    Recent discoveries in the field of education and learning
+                    have disproved the previously accepted notions of “optimal
+                    study practices”. Studies such as Butler (2010) provide
+                    suggestions for alternate, more effective strategies for
+                    learners to both gain and retain information. Such
+                    strategies include mind-mapping, spaced repetition, active
+                    recall and more. Additionally, surveys conducted in studies
+                    including Karpicke and Blunt (2011) illustrated that student
+                    perceptions of the best learning methods were vastly
+                    inaccurate to empirical evidence. Thus, there is an evident
+                    problem and opportunity for improvement surrounding the
+                    misallocation of resources by learners for meaningful
+                    studying.
                 </p>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing software
-                    like Aldus PageMaker including versions of Lorem Ipsum. Why
-                    do we use it? It is a long established fact that a reader
-                    will be distracted by the readable content of a page when
-                    looking at its layout. The point of using Lorem Ipsum is
-                    that it has a more-or-less normal distribution of letters,
-                    as opposed to using 'Content here, content here', making it
-                    look like readable English. Many desktop publishing packages
-                    and web page editors now use Lorem Ipsum as their default
-                    model text, and a search for 'lorem ipsum' will uncover many
-                    web sites still in their infancy. Various versions have
-                    evolved over the years, sometimes by accident, sometimes on
-                    purpose (injected humour and the like).
+                <p className={styles.text}>
+                    The main objective of this project is twofold and involves
+                    firstly, conducting further, detailed research on the
+                    “optimal” learning practices and secondly, to utilise the
+                    findings of this research to create an online, collaborative
+                    platform and web application for students and learners to
+                    gain and retain knowledge in the most efficient way
+                    possible.
                 </p>
             </PageLayout>
         </DefaultLayout>
