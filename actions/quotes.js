487d0955ae2d5361236854d981e63fdea2a96e18
Michael Kim <michaelkim39@gmail.com>
2021-04-26 14:46:33 +0100

    Fixed invisible quotes issue

--- actions/quotes.js
+++ actions/quotes.js
@@ -4,7 +4,7 @@ import { log } from '@/utils/logger';
 
 export const getMotivationalQuote = async () => {
     const response = await fetch(
-        'http://quotes.rest/qod.json?category=inspire',
+        'https://quotes.rest/qod.json?category=inspire',
     );
     const quoteObj = await response.json();
     return quoteObj.contents.quotes[0].quote;
